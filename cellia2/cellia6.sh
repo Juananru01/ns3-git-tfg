08df8dcce9988b50dce136be3f6778b7a27dee62
Juan Antonio Ruiz <ruizandujarja93@gmail.com>
2024-06-22 19:44:46 +0200

    Añadidos una nueva adaptación del script para ejecutar más escenarios

---
+++ cellia2/cellia6.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+
+if [ $# -ne 3 ]; then
+	echo "Illegal number of parameters"
+	echo "2: Start of seeds"
+	echo "3: End of seeds"
+	echo "4: FrequencyBand"
+	exit 2
+fi
+
+runs=`seq $1 1 $2`
+
+for run in $runs
+do
+	sbatch ./job6.sh $run $3
+done
---
+++ script2stas_more.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+
+seed=$1
+frequencyBand=$2
+
+if [ "$frequencyBand" == "2.4" ]; then
+    maxChannelWidth=40
+elif [ "$frequencyBand" == "5" ]; then
+    maxChannelWidth=160
+elif [ "$frequencyBand" == "6" ]; then
+    maxChannelWidth=160
+else
+    echo "Valor de frequencyBand no válido. Debe ser 2.4, 5 o 6."
+    exit 1
+fi
+
+# Encabezado del archivo CSV
+echo "Mean_Goodput(Mbps),NumSTAs,FrequencyBand(GHz),ChannelWidth(MHz),Seed,SimulationTime(s)" > goodput_vs_stas_802.11be_minstrel_maxWidthChannels_$frequencyBand'GHz'_gi800_seed$seed'_more'.csv
+
+# Recorrer cada valor de channelWidth
+for numStations in {155..195..10}; do
+    # Ejecutar el comando y almacenar la salida en una variable
+    output=$(./ns3 run "scratch/goodput_vs_stas_802.11be_minstrel --numStations=$numStations --channelWidth=$maxChannelWidth --frequency=$frequencyBand --simulationTime=25 --guard_interval=800 --seed=$seed");
+    
+    # Extraer los datos relevantes y escribirlos en el archivo CSV
+    meanThroughput=$(echo "$output" | grep -oP 'Throughput medio TOTAL de la simulación: \K[0-9.]+')
+    simulationTime=$(echo "$output" | grep -oP 'Simulation Time: \K[0-9]+')
+    
+    echo "$meanThroughput,$numStations,$frequencyBand,$maxChannelWidth,$seed,$simulationTime" >> goodput_vs_stas_802.11be_minstrel_maxWidthChannels_$frequencyBand'GHz'_gi800_seed$seed'_more'.csv
+done
+
+
+
+
