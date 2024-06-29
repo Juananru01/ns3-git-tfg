4bbb43edbe46fd8b295df49c59d0c0785944e7f6
Juan Antonio Ruiz <ruizandujarja93@gmail.com>
2024-06-29 12:52:59 +0200

    Modificado el escenario goodput vs distance con minstrel para lanzar multiples ejecuciones

---
+++ cellia_9/cellia9.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+
+if [ $# -ne 2 ]; then
+	echo "Illegal number of parameters"
+	echo "2: Start of seeds"
+	echo "3: End of seeds"
+	exit 1
+fi
+
+runs=`seq $1 1 $2`
+
+for run in $runs
+do
+	sbatch ./job20.sh $run
+done
