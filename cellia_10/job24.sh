a49604ede2b7630f6f726faea830642c2926fcd9
Juan Antonio Ruiz <ruizandujarja93@gmail.com>
2024-06-29 14:08:07 +0200

    Adaptados los escenarios goodput vs mcs para ejecutar varias simulaciones

---
+++ cellia_10/job24.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBATCH --output=%A.out
+#SBATCH --error=%A.err
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=7-48:00:00
+#SBATCH --partition=batch
+
+if [ $# -ne 1 ]; then
+	echo "Illegal number of parameters"
+	echo "1: Seed"
+	exit 1
+fi
+
+srun ../script1_goodput_mcs.sh $1
---
+++ script3_goodput_mcs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+
+seed=$1
+
+# Encabezado del archivo CSV
+echo "MCS,CtlrRate,Mean_Goodput(Mbps),Distance(m),ChannelWidth(Hz),GI(ns),SimulationTime(s),Seed" > goodput_vs_mcs_802.11ax_2.4GHz_seed$seed.csv
+
+# Definir los valores de channelWidth
+channelWidths=(20 40 80 160)
+gis=(800 3200)
+
+# Recorrer cada valor de channelWidth
+for gi in "${gis[@]}"; do
+    for channelWidth in "${channelWidths[@]}"; do
+        for mcs in {0..11}; do
+            # Ejecutar el comando y almacenar la salida en una variable
+            output=$(../ns3 run "scratch/throughputvalue_vs_distance_802.11ax_v2 --mcs=$mcs --channelWidth=$channelWidth --distance=3 --frequency=2.4 --simulationTime=35 --guard_interval=$gi --seed=$seed");
+            
+            # Extraer los datos relevantes y escribirlos en el archivo CSV
+            mcs=$(echo "$output" | grep -oP 'MCS: HeMcs\K[0-9]+')
+            ctrlRate=$(echo "$output" | grep -oP 'CtrlRate: \K[^,]+')
+            meanThroughput=$(echo "$output" | grep -oP 'Throughput medio simulación: \K[0-9.]+')
+            distance=$(echo "$output" | grep -oP 'Distance: \K[0-9.]+')
+            simulationTime=$(echo "$output" | grep -oP 'Simulation Time: \K[0-9]+')
+            
+            echo "$mcs,$ctrlRate,$meanThroughput,$distance,$channelWidth,$gi,$simulationTime,$seed" >> goodput_vs_mcs_802.11ax_2.4GHz_seed$seed.csv
+        done
+    done
+done
